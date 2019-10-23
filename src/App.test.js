20c333d3f7135074073f538c6ff4d536128cbeaa
Nick B <nicholas.balestrieri@gmail.com>
2019-10-23 15:34:28 -0400

    add comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { add, total } from "./App.js";
 
+// Unit tests for add function
 test("add function adds positive numbers", () => {
   expect(add(2, 2)).toBe(4);
 });
@@ -8,10 +9,12 @@ test("add function works with negative numbers", () => {
   expect(add(-5, -10)).toBe(-15);
 });
 
+// Sanity test
 test("addition works in general", () => {
   expect(2 + 2).toBe(4);
 });
 
+// Integration test, as total relies on add
 test("total shows the sum", () => {
   expect(total(5, 100)).toBe("$105");
 });
