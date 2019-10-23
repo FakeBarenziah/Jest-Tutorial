bca0c661befcc7b80aad27c857953a5ad961946c
Nick B <nicholas.balestrieri@gmail.com>
2019-10-23 15:29:32 -0400

    add integration test to test two functions

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
 export const add = (x, y) => x + y;
+
+export const total = (subtotal, tax) => {
+  return "$" + add(subtotal, tax);
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { add } from "./App.js";
+import { add, total } from "./App.js";
 
 test("add function adds positive numbers", () => {
   expect(add(2, 2)).toBe(4);
@@ -10,4 +10,8 @@ test("add function works with negative numbers", () => {
 
 test("addition works in general", () => {
   expect(2 + 2).toBe(4);
+});
+
+test("total shows the sum", () => {
+  expect(total(5, 100)).toBe("$105");
 });
